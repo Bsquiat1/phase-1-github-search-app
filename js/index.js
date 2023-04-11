6ea9afd0980d26b5e2323ee664f7c255f39852bd
Denis <denis.were@student.moringaschool.com>
2023-04-11 15:06:27 +0300

    complete

--- js/index.js
+++ js/index.js
@@ -1,68 +1,63 @@
-const form = document.querySelector('#github-form');
-form.addEventListener('submit', hanldeSearch);
 
-function handlesearch(event) {
-    event.preventDefault();
-    const searchTerm = document.querySelector('#search').ariaValueMax;
-    fetch(`https://api.github.com/search/users?q=${searchTerm}`)
-    .then(response => response.json())
-    .then(data => {
+// DOM Elements
+const searchForm = document.querySelector('#github-form');
+const searchInput = document.querySelector('#search');
+const userList = document.querySelector('#user-list');
+const reposList = document.querySelector('#repos-list');
 
-    })
-}
-function handleSearch(event) {
-    event.preventDefault();
-    const searchTerm = document.querySelector('#search').value;
-    fetch(`https://api.github.com/search/users?q=${searchTerm}`)
-      .then(response => response.json())
-      .then(data => {
-        const userList = document.querySelector('#user-list');
-        userList.innerHTML = '';
-        data.items.forEach(user => {
-          const li = document.createElement('li');
-          const avatar = document.createElement('img');
-          const username = document.createElement('span');
-          const link = document.createElement('a');
-  
-          avatar.src = user.avatar_url;
-          avatar.alt = `${user.login}'s avatar`;
-          username.textContent = user.login;
-          link.href = user.html_url;
-          link.textContent = 'Profile';
-  
-          li.appendChild(avatar);
-          li.appendChild(username);
-          li.appendChild(link);
-          userList.appendChild(li);
-  
-          li.addEventListener('click', () => {
-            
-            fetch(`https://api.github.com/users/${user.login}/repos`)
-              .then(response => response.json())
-              .then(repos => {
-                
-              })
-            
+// GitHub API Endpoints
+const userSearchEndpoint = 'https://api.github.com/search/users?q=';
+const userReposEndpoint = 'https://api.github.com/users/';
+
+// Event Listener for Form Submission
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent form from submitting
+
+  // Get Search Query
+  const searchQuery = searchInput.value.trim();
+
+  // Check if Search Query is Not Empty
+  if (searchQuery) {
+    // Clear Previous Search Results
+    userList.innerHTML = '';
+    reposList.innerHTML = '';
+
+    // Search for Users
+    fetch(userSearchEndpoint + searchQuery)
+      .then((response) => response.json())
+      .then((data) => {
+        const users = data.items;
+
+        // Display Search Results
+        users.forEach((user) => {
+          const userCard = document.createElement('li');
+          userCard.innerHTML = `
+            <img src="${user.avatar_url}" alt="${user.login}">
+            <a href="${user.html_url}" target="_blank">${user.login}</a>
+          `;
+          userCard.addEventListener('click', () => {
+            // Get User Repos
+            fetch(userReposEndpoint + user.login + '/repos')
+              .then((response) => response.json())
+              .then((data) => {
+                const repos = data;
+
+                // Display User Repos
+                reposList.innerHTML = '';
+                repos.forEach((repo) => {
+                  const repoCard = document.createElement('li');
+                  repoCard.innerHTML = `
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                  `;
+                  reposList.appendChild(repoCard);
+                });
+              });
           });
+          userList.appendChild(userCard);
         });
       })
-     
+      .catch((error) => {
+        console.error(error);
+      });
   }
-  li.addEventListener('click', () => {
-    fetch(`https://api.github.com/users/${user.login}/repos`)
-      .then(response => response.json())
-      .then(repos => {
-        const reposList = document.querySelector('#repos-list');
-        reposList.innerHTML = '';
-        repos.forEach(repo => {
-          const li = document.createElement('li');
-          const link = document.createElement('a');
-          link.href = repo.html_url;
-          link.textContent = repo.name;
-          li.appendChild(link);
-          reposList.appendChild(li);
-        });
-      })
-     
-  });
-  
+});
